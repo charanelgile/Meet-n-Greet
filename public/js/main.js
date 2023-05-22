35ce433314b01a9bd83e28321538eaf6265c5af4
charanelgile <charanelgile@icloud.com>
2023-05-22 23:36:29 +0800

    On Button Click, Copy Personal Code to Clipboard

--- public/js/main.js
+++ public/js/main.js
@@ -6,3 +6,13 @@ const socket = io("/");
 
 // Pass connection to registerSocketEvents() from "./wss.js"
 wss.registerSocketEvents(socket);
+
+// On Button Click, copy User's Personal Code to Clipboard
+const personalCodeCopyButton = document.getElementById(
+  "personal_code_copy_button"
+);
+personalCodeCopyButton.addEventListener("click", () => {
+  const personalCode = store.getState().socketId;
+
+  navigator.clipboard && navigator.clipboard.writeText(personalCode);
+});
